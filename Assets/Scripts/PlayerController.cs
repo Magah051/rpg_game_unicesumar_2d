2f9b784969e0c491218c05147ab0597b3b6b68a1
José Magalhães <joses.magalhaes@hotmail.com>
2024-08-06 22:03:36 -0300

    :construction: Finish Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,12 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     public float velocidade = 5f;
     private Rigidbody2D rb;
+    public Animator playerAnimator;
+    bool isWalking = false;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        isWalking = false;
     }
 
     // Update is called once per frame
@@ -17,7 +20,15 @@ public class PlayerController : MonoBehaviour
     {
         float eixoX = Input.GetAxisRaw("Horizontal") * velocidade;
         float eixoY = Input.GetAxisRaw("Vertical") * velocidade;
+        isWalking = eixoX != 0 || eixoY != 0;
 
         rb.velocity = new Vector2(eixoX,eixoY);
+
+        if (isWalking)
+        {
+            playerAnimator.SetFloat("eixoX", eixoX);
+            playerAnimator.SetFloat("eixoY", eixoY);
+        }
+        playerAnimator.SetBool("isWalking", isWalking);
     }
 }
